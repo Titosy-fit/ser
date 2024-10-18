e6bacae17710fca3ba3b48e4f7296ce173e6237b
koto <koto@gmail.com>
2024-10-18 08:44:45 +0200

    nasina barrer de recherche

--- www/other/menu.js
+++ www/other/menu.js
@@ -1,1 +1,18 @@
+function filterMenu() {
+    const searchBar = document.getElementById('searchBar');
+    const filter = searchBar.value.toLowerCase();
+    const menu = document.getElementById('menu');
+    const menuItems = menu.getElementsByClassName('menu-items');
+
+    for (let i = 0; i < menuItems.length; i++) {
+        const item = menuItems[i];
+        const textValue = item.textContent || item.innerText;
+        
+        if (textValue.toLowerCase().indexOf(filter) > -1) {
+            item.style.display = "";
+        } else {
+            item.style.display = "none";
+        }
+    }
+}
 
